b4f34ef336e19048f525ec0e40cb6d8381bfb051
vib1240n <vibhorsgr@gmail.com>
2022-11-05 12:24:31 -0700

    fancify landing page and added assignment 4

---
+++ Assignment-4/fancymytext.js
@@ -0,0 +1,11 @@
+//create alert in
+//wrap alert in function
+function alert() {
+  alert("Hello World");
+  onclick = "alert()";
+}
+
+//create function to make text bigger
+function bigger() {
+  document.getElementById("text").style.fontSize = "24pt";
+}
